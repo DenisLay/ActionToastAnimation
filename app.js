0b003af1b25b90eabaaf77b7d9521c67503bb42b
Denis Kobzar <laycode00700@gmail.com>
2024-11-06 15:44:26 +0200

    toast done

--- app.js
+++ app.js
@@ -1,3 +1,35 @@
 window.onload = function() {
+    const root = document.querySelector('body');
+
+    mountToastContainer(root);
+    showToast('1 message');
+    showModal('content');
+}
+
+function mountToastContainer(root) {
+    const toastsContainer = document.createElement('div');
+    toastsContainer.classList.add('toasts-container');
+
+    root.appendChild(toastsContainer);    
+}
+
+function showToast(message, duration = 3000) {
+    const container = document.querySelector('.toasts-container');
+
+    const toast = document.createElement('div');
+    toast.classList.add('toast');
+    toast.innerHTML = `<span>${message}</span>`;
     
+    container.appendChild(toast);
+
+    setTimeout(() => {
+        toast.classList.add('show');
+    }, 400);
+
+    toast.addEventListener('click', function() {
+        toast.classList.remove('show');
+        setTimeout(function () {
+            toast.remove();
+        }, 400);
+    });
 }
